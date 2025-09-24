40960c9fceb30ab8e4b8beaa90f360af569d2e9b
divyamtalwar <divyamtalwar0@gmail.com>
2025-09-24 19:04:22 +0530

    adding testing file

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from langchain_google_genai import GoogleGenerativeAI
 
-llm = GoogleGenerativeAI(api_key="AIzaSyD7DB3FvAlE5lS_5Ezt91WhOX_EgqmixjM" , model="gemini-2.5-flash")
-a = llm.invoke("WHAT IS THIS")
+#llm = GoogleGenerativeAI(api_key="AIzaSyAU4sn6k_iC-DZRfX1cwg1Uy_fT9PmttCM" , model="gemini-2.5-pro")
+#a = llm.invoke("HII BROTHER HOW RE YOU DOING TODYU!!!!")
 print(a)
 
